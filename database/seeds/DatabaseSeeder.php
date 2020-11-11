147ad694480e41892d7eca97d5aeb1d88701b2be
Artem_AS <voleeen@gmail.com>
2020-11-11 11:47:52 +0200

    add posts/seeder posts

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        factory(App\Models\User::class, 20)->create()->each(function($user) {
+            $user->posts()->saveMany(factory(App\Models\Post::class, 10)->make());
+        });
     }
 }
